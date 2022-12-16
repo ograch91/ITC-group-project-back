8c2068434a278b819e78f1ae75deadf332b3303a
Amnon Carmi <ace7884@hotmail.com>
2022-12-16 22:11:15 +0200

    getAllUsers Route implemented

--- Routes/users.route.js
+++ Routes/users.route.js
@@ -9,7 +9,7 @@
  */
 
 const express = require('express');
-const { login, signUp, updateUser, setUserImage, getUserById, ping } = require('../controllers/users.controller');
+const { login, signUp, updateUser, setUserImage, getUserById, getAllUsers,ping } = require('../controllers/users.controller');
 //  const { login, signUp, ping, updateUser, setUserImage} = require('../controllers/users.controller');
 const route = express.Router();
 const { authanticate } = require('../lib/JWT');
@@ -22,6 +22,8 @@ route.post('/signup', validateSchema(userDetailSchema), signUp);
 route.put('/update', authanticate, validateSchema(userDetailSchema), updateUser);
 route.get('/ping',authanticate, ping);
 route.put('/setphoto', authanticate, validateSchema(userImageUpdate), setUserImage);
+
+route.get('/getall', authanticate, getAllUsers);  
 route.get('/:id', authanticate, getUserById)  // MUST BE LAST ROUTE
 
 module.exports = route;
