e06eb0fffb648f6d0ae25e2a3cd0ff9a912ca763
Oren Grach <oren.grach@gmail.com>
2022-12-18 19:09:05 +0200

    WIP

--- Routes/users.route.js
+++ Routes/users.route.js
@@ -1,4 +1,4 @@
-/** 
+/**
  * ENDPOINTS:
  * login
  * signup
@@ -9,21 +9,52 @@
  */
 
 const express = require('express');
-const { login, signUp, updateUser, setUserImage, getUserById, getAllUsers,ping } = require('../controllers/users.controller');
+const {
+  login,
+  signUp,
+  updateUser,
+  setUserImage,
+  getUserById,
+  getAllUsers,
+  ping,
+} = require('../controllers/users.controller');
 //  const { login, signUp, ping, updateUser, setUserImage} = require('../controllers/users.controller');
 const route = express.Router();
 const { authanticate } = require('../lib/JWT');
-const { userLoginSchema, userDetailSchema, userImageUpdate } = require('../validation/users.schema');
+const users = require('../Lib/websock');
+const {
+  userLoginSchema,
+  userDetailSchema,
+  userImageUpdate,
+} = require('../validation/users.schema');
 //  const { userLoginSchema, userDetailSchema, userImageUpdate } = require('../validation/users.schema');
 const { validateSchema } = require('../validation/validate');
 
+route.get('/wstest', (req, res) => {
+  [...users.cons]
+    // .filter(u => u.user.email == user.email)
+    .forEach(u => u.ws.send('recieved' + u.user.email));
+  console.log(users);
+  res.send('ok');
+});
+
 route.post('/login', validateSchema(userLoginSchema), login);
 route.post('/signup', validateSchema(userDetailSchema), signUp);
-route.put('/update', authanticate, validateSchema(userDetailSchema), updateUser);
-route.get('/ping',authanticate, ping);
-route.put('/setphoto', authanticate, validateSchema(userImageUpdate), setUserImage);
+route.put(
+  '/update',
+  authanticate,
+  validateSchema(userDetailSchema),
+  updateUser
+);
+route.get('/ping', authanticate, ping);
+route.put(
+  '/setphoto',
+  authanticate,
+  validateSchema(userImageUpdate),
+  setUserImage
+);
 // Authentication bybassed for /getAllUsers for dev porpuses untill fix to work with authenticate
-route.get('/getall',/*authanticate, */ getAllUsers);  
-route.get('/:id', authanticate, getUserById)  // MUST BE LAST ROUTE
+route.get('/getall', /*authanticate, */ getAllUsers);
+route.get('/:id', authanticate, getUserById); // MUST BE LAST ROUTE
 
 module.exports = route;
